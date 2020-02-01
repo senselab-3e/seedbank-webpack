4bc02946e03d5bfe52936fc08052d63bc5dc1145
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-01 06:15:23 +0000

    ok. so In css-loader 3.0.0, there was a breaking change with the localIdentName configuration option but webpack requires cssloader so i modified the way the the webpackconfigjs file cites this using notes from github.com/rails/webpacker/issues/2197

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,9 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             importLoaders: 1,
-                            modules: true,
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                            modules: {
+                                localIdentName: "[name]__[local]___[hash:base64:5]",
+                            },	
                         }
                      },
                      { 
